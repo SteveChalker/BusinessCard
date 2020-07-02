dc1b703f66359cf910f483fa08955c4c7c9e6339
Stephen Chalker <stephen@bypassmobile.com>
2020-07-02 17:50:47 -0500

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Stephen Chalker on 7/2/20.
+//  Copyright © 2020 Stephen Chalker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
